d725ae3fbae5043d8be04e3d3f282e1968bbbf8e
Hawty1 <marius_erpenbach@web.de>
2021-05-04 03:27:32 +0200

    keyboard highlighting on keydown and unlight on keyup

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
 function highlightKey(e){
-    console.log(e.code)
+    var keys = document.getElementsByTagName("p");
     // search keyboard tables for keypress matches
+    for(i=0;keys.length>i;i++){
+        if (e.key.toUpperCase()==keys[i].innerHTML){
+           keys[i].style ="background-color:yellow;";     
+        }
+    }
 };
-
-
+function unlightKey(e){
+    var keys = document.getElementsByTagName("p");
+    // search keyboard tables for keypress matches
+    for(i=0;keys.length>i;i++){
+        if (e.key.toUpperCase()==keys[i].innerHTML){
+           keys[i].style ="background-color:;";     
+        }
+    }
+};
 const body = document.querySelector("body");
-body.addEventListener("keypress", highlightKey);
+body.addEventListener("keydown", highlightKey);
+body.addEventListener("keyup",unlightKey);
